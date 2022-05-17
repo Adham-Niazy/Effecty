87bad84780c782f9267a7857043eabe609a89a2b
Adham-Niazy <adhamniazy@gmail.com>
2022-05-17 21:00:38 +0200

    Adding some enhancements to UX

--- public/main.js
+++ public/main.js
@@ -1,28 +1,38 @@
 async function init() {
-  let rustApp = null
+  let rustApp = null;
 
   try {
-    rustApp = await import('../pkg')
-  } catch(err) {
-    console.error(err)
+    rustApp = await import("../pkg");
+  } catch (err) {
+    console.error(err);
     return;
   }
-  const input = document.getElementById('upload')
-  const fileReader = new FileReader()
+  const input = document.getElementById("upload");
+  const error = document.getElementById("error");
+  const loading = document.getElementById("loading");
+  const fileReader = new FileReader();
 
   fileReader.onloadend = () => {
+    error.classList.add("hidden");
     let base64 = fileReader.result.replace(
-      /^data:image\/(png|jpeg|jpg);base64,/, ''
-    )
-    let img_data_url = rustApp.grayscale(base64)
-    document.getElementById('new-img').setAttribute(
-      'src', img_data_url
-    )
-  }
+      /^data:image\/(png|jpeg|jpg);base64,/,
+      ""
+    );
+    let img_data_url = rustApp.grayscale(base64);
+    loading.classList.add("hidden");
+    document.getElementById("new-img").setAttribute("src", img_data_url);
+  };
 
-  input.addEventListener('change', () => {
-    fileReader.readAsDataURL(input.files[0])
-  })
+  input.addEventListener("change", () => {
+    error.classList.add("hidden");
+    document.getElementById("new-img").setAttribute("src", "");
+    if (input.files[0].type === "image/png") {
+      loading.classList.remove("hidden");
+      fileReader.readAsDataURL(input.files[0]);
+    } else {
+      error.classList.remove("hidden");
+    }
+  });
 }
 
-init()
+init();
